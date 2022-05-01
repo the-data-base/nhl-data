b308f021ca506cf53c6f8aa0a4e3f9a66a74efc8
dmf95 <domenicfayad@gmail.com>
2022-05-01 15:41:41 -0700

    Added f_player_season.sql, added a todo for live_plays staging

--- models/analytics/core/f_player_season.sql
+++ models/analytics/core/f_player_season.sql
@@ -6,7 +6,7 @@ player_season as (
         ,season.id as season_id
         ,season.regular_season_start_date
         ,season.regular_season_end_date
-        ,season.number_of_games full_season_games
+        ,season.number_of_games as full_season_games
         ,count(distinct bp.game_id) as boxscore_games
         ,sum(ifnull(bp.assists, 0)) as assists
         ,sum(ifnull(bp.goals, 0)) as goals
@@ -28,8 +28,8 @@ player_season as (
         ,sum(ifnull(bp.saves, 0)) as saves
         ,sum(ifnull(bp.powerplay_saves, 0)) as powerplay_saves
         ,sum(ifnull(bp.even_saves, 0)) as even_saves
-        ,sum(ifnull(bp.short_handed_shots_against, 0)) as short_handed_shots_against
-        ,sum(ifnull(bp.even_shots_against, 0)) as even_shots_againstrplay_saves
+        ,sum(ifnull(bp.short_handed_shots_against, 0)) as shorthanded_shots_against
+        ,sum(ifnull(bp.even_shots_against, 0)) as even_shots_against
         ,sum(ifnull(bp.powerplay_shots_against, 0)) as powerplay_shots_against
         --,sum(case when bp.decision = "W" then 1 else 0) as wins
         --,sum(case when bp.decision = "L" then 1 else 0) as losses
@@ -37,6 +37,9 @@ player_season as (
         nhl-breakouts.dbt_dom.f_boxscore_player as bp
         left join nhl-breakouts.dbt_dom.d_schedule as schedule on schedule.game_id = bp.game_id
         left join nhl-breakouts.dbt_dom.d_seasons as season on season.id = schedule.season_id
+    where 1 = 1
+        and schedule.game_type = 'R'
+        and bp.time_on_ice is not null
     group by
         bp.player_id
         ,bp.player_full_name
@@ -49,38 +52,94 @@ player_season as (
         ,sum(bp.goals) desc
 )
 
+-- at the player-season level, get the number of missed shots and blocked shots by each shooter
+, player_shots as (
+    select  
+    plays.player_id
+    ,season.id as season_id
+    ,max(plays.event_description) as example_eventdescription
+    ,sum(case when plays.event_type = "BLOCKED_SHOT" and plays.player_role = "SHOOTER" then 1 else 0 end) as shots_blocked
+    ,sum(case when plays.event_type = "MISSED_SHOT" and plays.player_role = "SHOOTER" then 1 else 0 end) as shots_missed
+    ,sum(case when plays.event_type = "SHOT" and plays.player_role = "SHOOTER" then 1 else 0 end) as shots_saved
+    ,sum(case when plays.event_type = "GOAL" and plays.player_role = "SCORER" then 1 else 0 end) as shots_scored
+    ,sum(case when plays.last_goal_game_winning = 1 and plays.player_role = "SCORER" then 1 else 0 end) as goals_gamewinning
+    ,sum(case when (plays.home_result_of_play = 'Chase goal' or plays.away_result_of_play = 'Chase goal') and plays.player_role = "SCORER" then 1 else 0 end) as goals_chasegoal
+    ,sum(case when (plays.home_result_of_play = 'Tying goal scored' or plays.away_result_of_play = 'Tying goal scored') and plays.player_role = "SCORER" then 1 else 0 end) as goals_gametying
+    ,sum(case when (plays.home_result_of_play = 'Go-ahead goal scored' or plays.away_result_of_play = 'Go-ahead goal scored') and plays.player_role = "SCORER" then 1 else 0 end) as goals_goahead
+    ,sum(case when (plays.home_result_of_play = 'Buffer goal' or plays.away_result_of_play = 'Buffer goal') and plays.player_role = "SCORER" then 1 else 0 end) as goals_buffergoal
+from nhl-breakouts.dbt_dom.f_plays as plays
+    left join nhl-breakouts.dbt_dom.d_schedule as schedule on schedule.game_id = plays.game_id
+    left join nhl-breakouts.dbt_dom.d_seasons as season on season.id = schedule.season_id
+where 1 = 1
+    and plays.player_role in ("SHOOTER", "SCORER") 
+    and plays.event_type in ("BLOCKED_SHOT", "MISSED_SHOT", "SHOT", "GOAL")
+group by    
+    plays.player_id
+    ,season.id
+order by
+    sum(case when plays.event_type = "GOAL" and plays.player_role = "SCORER" then 1 else 0 end) desc
+)
+
 select 
+    /* Primary Key */
+    /* Foreign Keys */
     player_season.player_id
-    ,player_season.player_full_name
     ,player_season.season_id
+    /* Season Properties */
     ,player_season.regular_season_start_date
     ,player_season.regular_season_end_date
     ,player_season.full_season_games
     ,player_season.boxscore_games
+    /* Player Properties */
+    ,player_season.player_full_name
+    /* Player Stats */
+    -- Time on Ice
+    ,(player_season.time_on_ice_mins * 60) + (player_season.time_on_ice_seconds) as time_on_ice_seconds
+    ,round((player_season.time_on_ice_mins) + (player_season.time_on_ice_seconds / 60),2) as time_on_ice_minutes
+    ,round(((player_season.time_on_ice_mins) + (player_season.time_on_ice_seconds / 60)) / player_season.boxscore_games,2) as avg_time_on_ice_mins
+    -- Goal-scoring skater events (Goals, Assists, Points)
     ,player_season.goals
+    ,player_shots.goals_gamewinning
+    ,player_shots.goals_chasegoal
+    ,player_shots.goals_gametying
+    ,player_shots.goals_goahead
+    ,player_shots.goals_buffergoal
     ,player_season.assists
     ,player_season.goals + player_season.assists as points
+    -- Shooting skater events
     ,player_season.shots
-    ,player_season.hits
+    ,case when player_season.shots < 1 then 0 else round((player_season.goals / player_season.shots),2) end as pcnt_shooting
+    ,player_shots.shots_blocked
+    ,player_shots.shots_missed
+    ,player_shots.shots_saved
+    ,player_shots.shots_scored
+    -- Other skater events
     ,player_season.faceoff_wins
-    ,player_season.faceoff_taken
+    ,player_season.faceoff_taken    
+    ,case when player_season.faceoff_taken < 1 then 0 else round((player_season.faceoff_wins / player_season.faceoff_taken),2) end as pcnt_faceoffwins
+    ,player_season.hits
     ,player_season.takeaways
     ,player_season.giveaways    
     ,player_season.blocked
     ,player_season.plus_minus
     ,player_season.pim    
+    -- Special teams skater events
     ,player_season.powerplay_goals
     ,player_season.powerplay_assists
     ,player_season.short_handed_goals
     ,player_season.short_handed_assists
+    -- Goalie events
     ,player_season.saves
     ,player_season.powerplay_saves
     ,player_season.even_saves
-    ,player_season.short_handed_shots_against
-    ,player_season.even_shots_againstrplay_saves
+    ,player_season.shorthanded_shots_against
+    ,player_season.even_shots_against
     ,player_season.powerplay_shots_against
     --,player_season.wins
     --,player_season.losses
-    ,(player_season.time_on_ice_mins * 60) + (player_season.time_on_ice_mins) as time_on_ice_seconds
 from 
-    player_season;
+    player_season
+    left join player_shots on player_season.player_id = player_shots.player_id and player_season.season_id = player_shots.season_id
+order  by
+    player_season.goals desc
+    ;
