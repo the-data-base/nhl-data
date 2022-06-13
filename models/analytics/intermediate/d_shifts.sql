e9b7988c5393a569ac4d9b72e23ecc50864739a6
dmf95 <domenicfayad@gmail.com>
2022-06-12 22:37:41 -0700

    Added d_shifts, d_shifts_time (promoted the two from staging) and started YML files for each of those, todo

--- models/analytics/intermediate/d_shifts.sql
+++ models/analytics/intermediate/d_shifts.sql
@@ -1,1 +1,33 @@
---todo
+select
+    /* Primary Key */
+    shifts.shift_id
+
+    /* Identifiers */
+    , shifts.game_id
+    , shifts.player_id
+    , shifts.team_id
+
+    /* Shift Properties */
+    , shifts.shift_number
+    , shifts.start_time
+    , shifts.end_time
+    , shifts.duration
+    , shifts.duration_seconds_elapsed
+    , shifts.start_seconds_elapsed
+    , shifts.end_seconds_elapsed
+
+    /* Properties */
+    , shifts.period
+    , shifts.period_type
+    , shifts.home_away_team
+    , shifts.game_type_description
+    , shifts.event_number
+    , shifts.type_code
+    , shifts.detail_code
+    , shifts.player_full_name
+    , shifts.is_goal
+    , shifts.goal_game_state
+    , shifts.goal_assisters
+    , shifts.goal_primary_assister_full_name
+    , shifts.goal_secondary_assister_full_name
+from {{ ref('stg_nhl__shifts') }} as shifts
