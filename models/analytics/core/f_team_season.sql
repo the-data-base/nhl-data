79ad010b749ddf5757e3e50f4de51b679a2dd5ca
nkumar97 <64378275+nkumar97@users.noreply.github.com>
2022-09-10 21:44:15 -0400

    Add f_team_season to dbt (#65)
    * Created f_team_season still need to run dbt and sql linter tests
    
    * Ran sqlfluff fix, for some reason didnt work for nitish... made changes, also tested dbt and it ran as intended
    
    * Added game_type, and team_name, an order by, refactored commas a bit, and changed the yml file
    
    * Whoops - fixed sql thing caught in the linting
    
    Co-authored-by: nitishkumar <nitish.kumar@mail.utoronto.ca>
    Co-authored-by: dmf95 <domenicfayad@gmail.com>
    Co-authored-by: Domenic <48701668+dmf95@users.noreply.github.com>

---
+++ models/analytics/core/f_team_season.sql
@@ -0,0 +1,57 @@
+with t1 as (
+    select
+        boxscore.*
+        , schedule.season_id
+        , schedule.game_type_description
+        , schedule.game_type
+    from {{ ref('f_boxscore_team') }} as boxscore
+    left join {{ ref('d_schedule') }} as schedule
+        on boxscore.game_id = schedule.game_id
+)
+
+, team_season as (
+    select
+        team_id
+        , team_name
+        , season_id
+        , game_type
+        , game_type_description
+        , count(*) as games_played
+        , sum(case when team_winner = 'true' then 1 else 0 end) as wins
+        , sum(ifnull(team_goals, 0)) as team_goals
+        , sum(ifnull(team_goal_differential, 0)) as team_goal_differential
+        , sum(ifnull(team_pim, 0)) as team_pim
+        , sum(ifnull(team_shots, 0)) as team_shots
+        , round(sum(ifnull(team_powerplay_goals, 0)), 0) as team_powerplay_goals
+        , sum(ifnull(team_hits, 0)) as team_hits
+        , sum(ifnull(team_blocked, 0)) as team_blocked
+        , sum(ifnull(team_takeaways, 0)) as team_takeaways
+        , sum(ifnull(team_giveaways, 0)) as team_giveaways
+    from t1
+    group by 1, 2, 3, 4, 5
+)
+
+select
+    /* Primary Key */
+    {{ dbt_utils.surrogate_key(['team_season.team_id', 'team_season.season_id', 'team_season.game_type_description']) }} as team_season_stage_id
+    /* Foreign Keys */
+    , team_season.team_id
+    , team_season.season_id
+    /* Team & Season Properties */
+    , team_season.team_name
+    , team_season.game_type
+    , team_season.game_type_description
+    , games_played
+    /* Team Stats */
+    , wins
+    , team_goals
+    , team_goal_differential
+    , team_pim
+    , team_shots
+    , team_powerplay_goals
+    , team_hits
+    , team_blocked
+    , team_giveaways
+    , team_takeaways
+from team_season
+order by team_season.season_id, team_season.game_type
