1ab4d2030c8391eacea2a4fabf36cd8d3604678f
dmf95 <domenicfayad@gmail.com>
2022-09-10 17:09:28 -0700

    Ran sqlfluff fix, for some reason didnt work for nitish... made changes, also tested dbt and it ran as intended

--- models/analytics/core/f_team_season.sql
+++ models/analytics/core/f_team_season.sql
@@ -1,34 +1,35 @@
 with t1 as (
-select bs.*,
-  sch.season_id,
-  sch.game_type_description
-from {{ ref('f_boxscore_team') }} as bs
-left join {{ ref('d_schedule') }} as sch
-  on bs.game_id = sch.game_id
+    select
+        bs.*,
+        sch.season_id,
+        sch.game_type_description
+    from {{ ref('f_boxscore_team') }} as bs
+    left join {{ ref('d_schedule') }} as sch
+        on bs.game_id = sch.game_id
 )
 
 , team_season as (
-SELECT
-  team_id,
-  team_name,
-  season_id,
-  game_type_description,
-  count(*) as games_played,
-  SUM(CASE WHEN team_winner = 'true' THEN 1 ELSE 0 END) as wins,
-  sum(ifnull(team_goals, 0)) as team_goals,
-  sum(ifnull(team_goal_differential, 0)) as team_goal_differential,
-  sum(ifnull(team_pim, 0)) as team_pim,
-  sum(ifnull(team_shots, 0)) as team_shots,
-  sum(ifnull(team_powerplay_goals, 0)) as team_powerplay_goals,
-  sum(ifnull(team_hits, 0)) as team_hits,
-  sum(ifnull(team_blocked, 0)) as team_blocked,
-  sum(ifnull(team_takeaways, 0)) as team_takeaways,
-  sum(ifnull(team_giveaways, 0)) as team_giveaways
-FROM t1
-group by 1, 2, 3, 4
+    select
+        team_id,
+        team_name,
+        season_id,
+        game_type_description,
+        count(*) as games_played,
+        sum(case when team_winner = 'true' then 1 else 0 end) as wins,
+        sum(ifnull(team_goals, 0)) as team_goals,
+        sum(ifnull(team_goal_differential, 0)) as team_goal_differential,
+        sum(ifnull(team_pim, 0)) as team_pim,
+        sum(ifnull(team_shots, 0)) as team_shots,
+        sum(ifnull(team_powerplay_goals, 0)) as team_powerplay_goals,
+        sum(ifnull(team_hits, 0)) as team_hits,
+        sum(ifnull(team_blocked, 0)) as team_blocked,
+        sum(ifnull(team_takeaways, 0)) as team_takeaways,
+        sum(ifnull(team_giveaways, 0)) as team_giveaways
+    from t1
+    group by 1, 2, 3, 4
 )
 
-select 
+select
     /* Primary Key */
     {{ dbt_utils.surrogate_key(['team_season.team_id', 'team_season.season_id', 'team_season.game_type_description']) }} as team_season_stage_id
     /* Foreign Keys */
