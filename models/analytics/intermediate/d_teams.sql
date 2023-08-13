d072b8ea33b843cdbf1be15b4fc2d84291660d79
dmf95 <domenicfayad@gmail.com>
2023-08-13 15:58:19 -0700

    fuckin sqlfluff, cmon pls let this be it

--- models/analytics/intermediate/d_teams.sql
+++ models/analytics/intermediate/d_teams.sql
@@ -7,27 +7,27 @@ with deduplicated as (
 )
 
 select
-  /* Primary Key */
-  team_id
+    /* Primary Key */
+    team_id
 
-  /* Identifiers */
-  , venue_timezone_id
-  , division_id
-  , conference_id
-  , franchise_id
+    /* Identifiers */
+    , venue_timezone_id
+    , division_id
+    , conference_id
+    , franchise_id
 
-  /* Properties */
-  , full_name
-  , team_url
-  , venue_name
-  , venue_url
-  , venue_city
-  , venue_timezone_name
-  , venue_timezone_offset
-  , abbreviation
-  , team_name
-  , location_name
-  , first_year_of_play
-  , short_name
-  , is_active
+    /* Properties */
+    , full_name
+    , team_url
+    , venue_name
+    , venue_url
+    , venue_city
+    , venue_timezone_name
+    , venue_timezone_offset
+    , abbreviation
+    , team_name
+    , location_name
+    , first_year_of_play
+    , short_name
+    , is_active
 from deduplicated
